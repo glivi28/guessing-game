e4fe36b1cf48edc54be2292334fb1dbc57357001
Yaroslav Dekhtiaruk <dehtiaruk@gmail.com>
2023-02-27 16:08:42 +0200

    feat: implement task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+	constructor() {
+		this.min;
+		this.max;
+		this.actualNum;
+	}
 
-    setRange(min, max) {
+	setRange(min, max) {
+		this.min = min;
+		this.max = max;
 
-    }
+	}
 
-    guess() {
+	guess() {
+		let actualNum = Math.round((this.max + this.min) / 2);
+		this.actualNum = actualNum;
+		return this.actualNum;
+	}
 
-    }
+	lower() {
+			this.max = this.actualNum;
+	}
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	greater() {
+			this.min = this.actualNum;
+	}
 }
 
 module.exports = GuessingGame;
